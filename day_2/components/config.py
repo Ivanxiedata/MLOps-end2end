bce1ed76ad743f879796a9d97364e5e2a642287e
z0x01mw <ivan.xie@samsclub.com>
2024-12-02 15:09:22 -0600

    add new code

---
+++ day_2/components/config.py
@@ -0,0 +1,7 @@
+GCP_PROJECT = "dev-sams-ds-train"
+SERVICE_ACCOUNT = "svc-ds-train@dev-sams-ds-train.iam.gserviceaccount.com"
+NETWORK = "projects/12856960411/global/networks/vpcnet-private-svc-access-usc1"
+PIPELINE_ROOT = "gs://mlops-summit"
+PIPELINE_NAME = "hello world pipeline"
+CPU_LIMIT = '32'  # the CPU MAX limit to 96 CPUs
+MEMORY_LIMIT = '32G' # the memory MAX limit to 624 Gigabytes
